4b5c567c081017903062da08fa68fbe4d7246264
Abdulrahiman7 <rahimanabdul981@gmail.com>
2023-06-26 14:09:35 +0530

    new li branch

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var l=document.getElementById('main-header');
 l.style.borderBottom= 'solid 3px yellow';
-var ite=document.getElementsByTagName('li');
-ite[2].style.backgroundColor= 'green';
-ite[4].style.color= 'red';
 for(let i=0;i<ite.length;i++)
 {
 ite[i].style.fontWeight= 'bold';
 }
+var i=document.getElementsByClassName('list-group-item');
+i.style.color= 'red';
+var hea=document.querySelector('.list-group-item:last-child');
+hea.style.color= 'red';
+var th=document.querySelector('.list-group-item:nth-child(3)');
+th.style.display= 'none';
