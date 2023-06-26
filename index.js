4f9487a29bee7262958ac83f44d88eb860533607
Abdulrahiman7 <rahimanabdul981@gmail.com>
2023-06-26 12:11:52 +0530

    modified index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 var l=document.getElementById('main-header');
 l.style.borderBottom= 'solid 3px yellow';
+var ite=document.getElementsByClassName('list-group-item');
+ite[2].style.backgroundColor= 'green';
+for(let i=0;i<ite.length;i++)
+{
+ite[i].style.fontWeight= 'bold';
+}
