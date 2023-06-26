02651314a33c0ea1c7b2e5f29076b2b146d54c95
Abdulrahiman7 <rahimanabdul981@gmail.com>
2023-06-26 13:04:14 +0530

    new list of li

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var l=document.getElementById('main-header');
 l.style.borderBottom= 'solid 3px yellow';
-var ite=document.getElementsByClassName('list-group-item');
+var ite=document.getElementsByTagName('li');
 ite[2].style.backgroundColor= 'green';
+ite[4].style.color= 'red';
 for(let i=0;i<ite.length;i++)
 {
 ite[i].style.fontWeight= 'bold';
