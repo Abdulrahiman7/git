6de47b13828d60373a8759b88b92e2ef35acb971
Abdulrahiman7 <rahimanabdul981@gmail.com>
2023-06-27 21:49:09 +0530

    Add and Remove final form

--- newindex.js
+++ newindex.js
@@ -1,25 +1,39 @@
-var l = document.getElementById('main-header');
-l.style.borderBottom = 'solid 3px blue';
+var form = document.getElementById('addForm');
+var itemsList = document.getElementById('items');
 
-var ite = document.getElementsByClassName('list-group-item');
 
-for (let i = 0; i < ite.length; i++) {
-  ite[i].style.fontWeight = 'bold';
+form.addEventListener('submit', addItem);
+itemsList.addEventListener('click',delet);
+
+function addItem(e) {
+  e.preventDefault(); 
+
+  var li = document.createElement('li');
+  li.className = "list-group-item";
+  var newit = document.getElementById('item').value; 
+  li.appendChild(document.createTextNode(newit));
+
+  var delet=document.createElement('delete');
+  delet.className= "btn btn-danger btn-sm float-right delete";
+  delet.appendChild(document.createTextNode('X'));
+  li.appendChild(delet);
+  itemsList.appendChild(li);
 }
+function delet(e)
+{
+    if(e.target.classList.contains('delete'))
+    {
+        if(confirm('Are you sure?'))
+        {
+            var li=e.target.parentElement;
+            itemsList.removeChild(li);
+        }
+    }
+}
+var items = document.getElementsByClassName('list-group-item');
 
-
-var head = document.querySelector('.list-group-item:nth-child(2)');
-  head.style.color = 'green';
-
-  var th = document.querySelector('.list-group-item:nth-child(3)');
-  th.style.color = 'transparent';
-
-  var div=document.createElement('div');
-  var txt=document.createTextNode('HEllo');
-  div.appendChild(txt);
-  var bef=document.querySelector('header .container');
-  var h=document.querySelector('header h1');
-  bef.before(div,h);
-
-  var loc=document.querySelector('#items li:first-child');
-  loc.before(txt,loc);
+for (let i = 0; i < items.length; i++) {
+  var editButton = document.createElement('button');
+  editButton.textContent = 'Edit';
+  items[i].appendChild(editButton);
+}
