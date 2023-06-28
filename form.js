afb7cc65d052dc2e417b6cf1048beba5e43dc438
Abdulrahiman7 <rahimanabdul981@gmail.com>
2023-06-28 14:53:26 +0530

    name email and number

--- form.js
+++ form.js
@@ -1,13 +1,14 @@
-let i=0;
 var form=document.getElementById('newform');
 form.addEventListener('submit', newForm);
 function newForm(e)
 {
     e.preventDefault();
     var newitem={
-         a:document.getElementById('txt').value,
-         b:document.getElementById('tx').value
+         name:document.getElementById('name').value,
+         email:document.getElementById('email').value,
+         number:document.getElementById('number').value
     };
+    em=document.getElementById('email').value;
     var c=JSON.stringify(newitem);
-    localStorage.setItem(i++, c);
+    localStorage.setItem(em, c);
 }
