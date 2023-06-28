a912f6b4134060633526a53ad13f65c516e97734
Abdulrahiman7 <rahimanabdul981@gmail.com>
2023-06-28 17:18:32 +0530

    new

--- form.js
+++ form.js
@@ -1,14 +1,40 @@
 var form=document.getElementById('newform');
+
 form.addEventListener('submit', newForm);
+var ul=document.createElement('ul');
+
 function newForm(e)
 {
     e.preventDefault();
-    var newitem={
-         name:document.getElementById('name').value,
-         email:document.getElementById('email').value,
-         number:document.getElementById('number').value
-    };
-    em=document.getElementById('email').value;
-    var c=JSON.stringify(newitem);
-    localStorage.setItem(em, c);
+
+    let nm=document.getElementById('name').value;
+    let no=document.getElementById('number').value;
+    let em=document.getElementById('email').value;
+    
+    
+    var li=document.createElement('li');
+    li.setAttribute('email-data',em);
+    var text=document.createTextNode('name='+nm+'   no.='+no+" Submitted");
+    var del=document.createElement('button');
+    del.textContent="delete";
+    li.appendChild(text);
+    li.appendChild(del);
+    ul.appendChild(li);
+
+    del.addEventListener('click',function()
+    {
+            var de=this.parentElement;
+            ul.removeChild(de);
+            var ema=de.getAttribute('email-data');  
+        localStorage.removeItem(ema);
+    });
+form.appendChild(ul);
+var newitem={
+    name:nm,
+    email:em,
+    number:no
+};
+em=document.getElementById('email').value;
+var c=JSON.stringify(newitem);
+localStorage.setItem(em, c);
 }
