da2898dd37069634771402586e7d660f054611c6
Abdulrahiman7 <rahimanabdul981@gmail.com>
2023-06-28 08:17:29 +0530

    add remove and delete

--- newindex.js
+++ newindex.js
@@ -1,9 +1,17 @@
 var form = document.getElementById('addForm');
 var itemsList = document.getElementById('items');
+var filter= document.getElementById('filter');
+
+var bef = document.getElementById('sub');
+var ne = document.createElement('input');
+ne.className = "form-control mr-2";
+ne.id= 'des';
+form.insertBefore(ne, bef);
 
 
 form.addEventListener('submit', addItem);
 itemsList.addEventListener('click',delet);
+filter.addEventListener('keyup',searching);
 
 function addItem(e) {
   e.preventDefault(); 
@@ -11,7 +19,12 @@ function addItem(e) {
   var li = document.createElement('li');
   li.className = "list-group-item";
   var newit = document.getElementById('item').value; 
+  var neit= document.getElementById('des').value; 
+  var br=document.createElement('br');
+  
   li.appendChild(document.createTextNode(newit));
+  li.appendChild(br);
+  li.appendChild(document.createTextNode(neit));
 
   var delet=document.createElement('delete');
   delet.className= "btn btn-danger btn-sm float-right delete";
@@ -30,10 +43,18 @@ function delet(e)
         }
     }
 }
-var items = document.getElementsByClassName('list-group-item');
-
-for (let i = 0; i < items.length; i++) {
-  var editButton = document.createElement('button');
-  editButton.textContent = 'Edit';
-  items[i].appendChild(editButton);
+function searching(e){
+    var text=e.target.value.toLowerCase();
+    var items=itemsList.getElementsByTagName('li');
+    Array.from(items).forEach(function(item){
+        var ite=item.textContent;
+        if(ite.toLowerCase().indexOf(text)!= -1){
+            item.style.display= 'block';
+        }else 
+        {
+            item.style.display= 'none';
+        }
+    });
 }
+
+
